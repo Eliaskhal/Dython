71f5ed34a2cb5bc1291767ef19de4af383641689
Ilyas Khallouki <khalloukielias@gmail.com>
2024-02-12 21:22:56 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from sys import argv
-
-from translator import Translator
-from translator import Parser
+from translator import Translator, Parser
 
 global_vars = {}
 local_vars = {}
 
+def main(code, is_line, global_vars, local_vars):
+    parser = Parser(Translator, code, is_line)
+    return parser.parse(global_vars, local_vars)
+
 if __name__ == "__main__":
+    from sys import argv
+    
     if len(argv) > 1:
-        code = argv[1]
-        parser = Parser(Translator, code, False)
-        parser.parse({}, {})
+        with open(argv[1], 'r') as f:
+            code = f.read()
+        main(code, False, {}, {})
+    
     else: 
         while True:
-            parser = Parser(Translator, input('>>> '), True)
-            global_vars, local_vars = parser.parse(global_vars, local_vars)
-    
+            code = input('>>> ')
+            global_vars, local_vars = main(code, True, global_vars, local_vars)
